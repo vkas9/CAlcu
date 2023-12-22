bc288d194bf4bf08ae12448d27ded1d99b334474
vikasvishwakarma963 <vikasvishwakarma963@gmail.com>
2023-12-22 14:03:25 +0000

    jn

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,13 @@ import ClockSlogan from "./components/ClockSlogan";
 import "bootstrap/dist/css/bootstrap.min.css";
 import React from "react";
 function App() {
-  let foodItems=[]
-  return <>
-  <center>
+  let foodItems=[];
+  return <center>
     <Clock />
     <ClockSlogan/>
      
     <ClockTime items={foodItems} />
   </center>
-  </>
+  
 }
 export default App;  
--- src/components/ClockTime.jsx
+++ src/components/ClockTime.jsx
@@ -4,33 +4,33 @@ import styles from "./FoodContainer.module.css";
 import { useState } from "react";
 let ClockTime = () => {
     let time = new Date();
-   
-    let [items2,setTextState]=useState([]);
 
-    const handleOnChange=(event)=>{
-        if( event.target.value!="" && event.key==='Enter'){
-            let newItemList=[...items2,event.target.value];
-            event.target.value="";
+    let [items2, setTextState] = useState([]);
+
+    const handleOnChange = (event) => {
+        if (event.target.value != "" && event.key === 'Enter') {
+            let newItemList = [...items2, event.target.value];
+            event.target.value = "";
             setTextState(newItemList);
 
-        }  
+        }
     };
     return <><p>
         This is the current time: {time.toLocaleDateString()} - {time.toLocaleTimeString()}
     </p>
-        <FoodInput handleOnChange={handleOnChange}/>
-       
-        {items2.length===0? <h2>I am Still Hungry!!!</h2>:null}
+        <FoodInput handleOnChange={handleOnChange} />
+
+        {items2.length === 0 ? <h2>I am Still Hungry!!!</h2> : null}
         <div className={styles.FoodContainer}>
 
-        <ul className="list-group">{
-            items2.map((item)=>(
-                <Items key={item} foodItem={item}/>
-                
+            <ul className="list-group">{
+                items2.map((item) => (
+                    <Items key={item} foodItem={item} />
+
                 ))
-            } 
-        </ul>
-            </div>
+            }
+            </ul>
+        </div>
     </>
 }
 export default ClockTime;
